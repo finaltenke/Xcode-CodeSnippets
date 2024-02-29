5b4b38688d995ad7d879ef7765a908ae5785326b
Rick <rick.ke@innotech.me>
2024-02-29 10:55:46 +0800

    update scripts

--- scripts/core/folder_tool.py
+++ scripts/core/folder_tool.py
@@ -1,10 +1,7 @@
 import shutil
 import os
 
-def _copyFolder(source_folder, destination_folder):
-    # 展開目標文件夾路徑中的波浪號（~）
-    destination_folder = os.path.expanduser(destination_folder)
-
+def copyFolder(source_folder, destination_folder):
     # 檢查目標文件夾是否存在，如果不存在則創建
     if not os.path.exists(destination_folder):
         os.makedirs(destination_folder)
@@ -20,11 +17,3 @@ def _copyFolder(source_folder, destination_folder):
         # 使用shutil.copytree複製整個目錄樹，允許目標文件夾存在
         shutil.copytree(source_folder, destination_folder, symlinks=False, ignore=None, dirs_exist_ok=True)
         print(f'複製文件: {source_file} 到 {destination_file}')
-
-
-def copyFolderArray(source_folders, destination_folders):
-    # 使用循環遍歷source_folders和destination_folders，並調用copyFolder函數
-    for i in range(len(source_folders)):
-        source_folder = source_folders[i]
-        destination_folder = destination_folders[i]
-        _copyFolder(source_folder, destination_folder)
--- scripts/sync-to-local.py
+++ scripts/sync-to-local.py
@@ -1,3 +1,4 @@
+import os
 import core.folder_tool as tool
 import model
 
@@ -7,5 +8,14 @@ source_folders = model.git_folders
 # 目標文件夾路徑
 destination_folders = model.xcode_folders
 
-tool.copyFolderArray(source_folders, destination_folders)
+def copyFolderArray(source_folders, destination_folders):
+    # 使用循環遍歷source_folders和destination_folders，並調用copyFolder函數
+    for i in range(len(source_folders)):
+        # 展開目標文件夾路徑中的波浪號（~）
+        source_folder = source_folders[i]
+        destination_folder = os.path.expanduser(destination_folders[i])
+        tool.copyFolder(source_folder, destination_folder)
+
+
+copyFolderArray(source_folders, destination_folders)
 print('複製完成！')
